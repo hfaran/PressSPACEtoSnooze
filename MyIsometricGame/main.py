2d04104ac0e74e5adcaada5c332b04cf35b16a7c
hfaran <hamzafaran@hotmail.com>
2014-01-18 18:38:51 -0800

    Add images on screen

--- MyIsometricGame/main.py
+++ MyIsometricGame/main.py
@@ -1,19 +1,35 @@
 import pygame
+from pygame.locals import QUIT
 from MyIsometricGame.utils import utils
 
 def main():
+    # Init screen
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    image = pygame.image.load("assets/tiles/default.png")
-    image_2 = utils.convert_white_to_transparent(image)
+    pygame.display.set_caption('MyIsometricGame')
 
+    # Load images
+    image = utils.convert_white_to_transparent(
+        pygame.image.load("assets/tiles/default.png").convert_alpha())
+
+    # Fill background
     b = pygame.Surface(screen.get_size())
-    b=b.convert()
+    b=b.convert_alpha()
     b.fill((100,)*3)
 
+    #Blit to screen
+    screen.blit(b, (0,)*2)
+    pygame.display.flip()
+
     while True:
-        from time import sleep
-        sleep(1)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+
+        # Blit images
+        screen.blit(image, (20,20))
+        screen.blit(image, (-20, -20))
+        pygame.display.flip()
 
 if __name__ == '__main__':
     main()
