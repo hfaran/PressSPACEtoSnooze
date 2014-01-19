729959aa2e249d2c6b4eed67d660656318fb62d5
hfaran <hamzafaran@hotmail.com>
2014-01-18 19:58:43 -0800

    Click detection

--- MyIsometricGame/main.py
+++ MyIsometricGame/main.py
@@ -7,20 +7,21 @@ from random import randint, choice
 from itertools import chain
 
 
-COLORS = ["pink", "green", "purple", "orange"]
 def generate_floor(numx, numy):
+    """Create a tiled floor of size numx, numy"""
+    __colors = ["pink", "green", "purple", "orange"]
     def getx(x,y,w,h):
         return int((x+y)*w/2.0)
     def gety(x,y,w,h):
         return int((numy+x-y)*h/2.0)
+
     l = []
     for x in xrange(numx):
         for y in xrange(numy):
-            t = Tile(choice(COLORS))
+            t = Tile(choice(__colors))
             width, height = t.image.get_size()
             t.set_pos(getx(x,y,width,height), gety(x,y,width,height))
             l.append(t)
-    #return [[Tile(choice(COLORS), getx(x,y), gety(x,y)) for x in xrange(numx)] for y in xrange(y)])
     return l
 
 def main():
@@ -36,6 +37,14 @@ def main():
     bg = pygame.Surface(screen.get_size())
     bg.fill((100,)*3)
 
+    # Add tiles
+    screen.blit(bg, (0,)*2)
+    for t in tiles:
+        print t.get_pos()
+        screen.blit(t.image, t.get_pos())
+    pygame.display.flip()
+
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -43,15 +52,11 @@ def main():
             if event.type == MOUSEBUTTONDOWN:
                 x,y = event.pos
                 print x,y
+                for n, t in enumerate(tiles):
+                    if t.point_collision((x-t._x,y-t._y)):
+                        print n
+                        break
 
-        screen.blit(bg, (0,)*2)
-        for t in tiles:
-            print t.get_pos()
-            screen.blit(t.image, t.get_pos())
-
-
-
-        pygame.display.flip()
 
 if __name__ == '__main__':
     main()
--- MyIsometricGame/tile.py
+++ MyIsometricGame/tile.py
@@ -6,12 +6,19 @@ class Tile(object):
     def __init__(self, filename, x=0, y=0):
         self.image = utils.convert_white_to_transparent(
             pygame.image.load("assets/tiles/{}.png".format(filename)).convert_alpha())
-        self.x = x
-        self.y = y
+        self.mask = pygame.mask.from_surface(self.image)
+        self._x = x
+        self._y = y
 
     def get_pos(self):
-        return self.x, self.y
+        return self._x, self._y
 
     def set_pos(self, x, y):
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
+
+    def point_collision(self, pos):
+        try:
+            return self.mask.get_at(pos)
+        except IndexError:
+            return False
