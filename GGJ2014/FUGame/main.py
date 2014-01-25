b9c845e445dde1246b614c927b5b04ea5abdb53a
Dylan Dumesnil <dadumesnil@gmail.com>
2014-01-24 21:02:11 -0800

    Game runs in fullscreen at monitor's res

--- GGJ2014/FUGame/main.py
+++ GGJ2014/FUGame/main.py
@@ -7,5 +7,8 @@ from FUGame.constants import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((FU_WIDTH, FU_HEIGHT))
+    #screen = pygame.display.set_mode((FU_WIDTH, FU_HEIGHT))
+    screenInfo = pygame.display.Info()
+    print((screenInfo.current_w, screenInfo.current_h))
+    screen = pygame.display.set_mode((screenInfo.current_w, screenInfo.current_h), pygame.FULLSCREEN)
     pygame.display.set_caption('FUGame')
