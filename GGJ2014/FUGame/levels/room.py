f1fa5551c4bea88276bdee1b469f20d2cde61e04
hfaran <hamzafaran@hotmail.com>
2014-01-25 01:29:47 -0800

    stuff

---
+++ GGJ2014/FUGame/levels/room.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.locals import *
+
+
+class Room(object):
+
+    def __init__(self):
+        self.world = self.create_world()
+
+    def create_world(self):
+        # Create objects
+        chars = {
+            "guy": Character(
+                filename="main",
+                x=0,
+                y=0,
+                z=0,
+                fps=5,
+                speed=1
+            ),
+        }
+        world = World("room", "room_bg", None, chars, 0, 0)
+        return world
+
+    def handle_events(self, event):
+        handled = False
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                world.NPCs["guy"].move("L")
+                handled = True
+        return handled
