33544a5a3b0ebaea8637ca57dfffba3cd3af6fd0
Dylan Dumesnil <dadumesnil@gmail.com>
2014-01-24 23:21:33 -0800

    sprite has own dt and framte rate

--- GGJ2014/FUGame/main.py
+++ GGJ2014/FUGame/main.py
@@ -19,14 +19,24 @@ def main():
         (FU_WIDTH, FU_HEIGHT),
     )
     pygame.display.set_caption('FUGame')
-    char = Character("test", 0, 0)
+    char = Character("test", 0, 0, 5)
+
+    screen.blit(char.current_frame, char.get_pos())
+    pygame.display.flip()
     # Game loop
     while True:
+        char.update_dt()
         for event in pygame.event.get():
             if event.type in EVENT_MAP:
                 func = EVENT_MAP[event.type][0]
                 args = EVENT_MAP[event.type][1:]
                 func(*args)
+
+        if char.dt.microseconds > 1.0 / char.fps * 1000000:
+            print "HIIII"
+            char.next_frame()
+            screen.blit(char.current_frame, char.get_pos())
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
--- GGJ2014/FUGame/sprite.py
+++ GGJ2014/FUGame/sprite.py
@@ -1,12 +1,14 @@
 import pygame
 import os
+from FUGame.constants import *
+from datetime import datetime
 from FUGame.utils import utils
 
 
 class Sprite(object):
     """Sprite class
     """
-    def __init__(self, filename, x, y):
+    def __init__(self, filename, x, y, fps=FU_FRAME_RATE):
         # self.image = utils.convert_white_to_transparent(
         #     pygame.image.load(
         #         "assets/sprites/{}/{}.png".format(filename, filename)
@@ -15,8 +17,13 @@ class Sprite(object):
         self.name = filename
         self._x = x
         self._y = y
+        self.fps = fps
+        self.lastdt = datetime.now()
+        self.update_dt()
         self.anims = self.load_frames()
-        self.current_frame = list(self.anims.iteritems())[0][1][0]
+        self.current_anim = list(self.anims.iteritems())[0][0]
+        self.current_frame_num = 0
+        self.current_frame = self.anims[self.current_anim][self.current_frame_num]
         self.mask = pygame.mask.from_surface(self.current_frame)
 
     def get_pos(self):
@@ -28,18 +35,23 @@ class Sprite(object):
         self._x = x
         self._y = y
 
+    def update_dt(self):
+        self.dt = datetime.now() - self.lastdt
+
+    def next_frame(self):
+        self.lastdt = datetime.now()
+        self.current_frame_num = (self.current_frame_num + 1) % len(self.anims[self.current_anim])
+        self.current_frame = self.anims[self.current_anim][self.current_frame_num]
+
     def load_frames(self):
         path = os.path.join("assets", "sprites", self.name)
         l = os.listdir(path)
         d = {
             folder_name: [pygame.image.load(
                 os.path.join(path, folder_name, image)) for image in os.listdir(
-                os.path.join(path, l))] for folder_name in l
+                os.path.join(path, folder_name))] for folder_name in l
         }
-        # d = {}
-        # for folder_name in l:
-        #     for image in os.path.join(path, folder_name):
-        #         d[folder_name]
+        print(d)
         return d
 
     def point_collision(self, pos):
