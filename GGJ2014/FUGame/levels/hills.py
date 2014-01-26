4543fbae269f7d8fa717f390c784686b87a37df9
Phattrick Tran <phattrick.tran@gmail.com>
2014-01-26 02:01:48 -0800

    Hills bounding

--- GGJ2014/FUGame/levels/hills.py
+++ GGJ2014/FUGame/levels/hills.py
@@ -27,19 +27,20 @@ class Hills(Level, EventHandlerMixin):
 
     def __init__(self):
         self.world = self.create_world()
+        self.allow_move = True
 
     def create_world(self):
         chars = {
             "guy": Character(
                 filename="main",
-                x=525,
-                y=190,
+                x=120,
+                y=260,
                 z=0,
                 col_pts=[],
                 col_x_offset=50,
                 col_y_offset=117,
                 fps=10,
-                speed=0.2
+                speed=7
             ),
         }
         statics = {
@@ -50,10 +51,15 @@ class Hills(Level, EventHandlerMixin):
             bg_filename="hills_bg",
             static=statics,
             NPCs=chars,
-            col_pts=[],
+            col_pts=[(65, 420), (107, 406), (127, 367), (159, 336), (187, 306), (224, 288), (253, 271), (281, 254),
+                     (310, 244), (386, 214), (437, 201), (472, 200), (539, 184), (619, 188), (621, 231), (617, 257),
+                     (626, 292), (626, 332), (626, 356), (626, 404), (626, 429), (626, 461), (626, 501), (626, 557),
+                     (626, 578), (626, 600), (626, 622), (626, 629), (626, 637)],
             x=0,
             y=0
         )
+
+        world.NPCs["guy"].set_anim("F")
         return world
 
     def handle_events(self, event):
@@ -75,3 +81,5 @@ class Hills(Level, EventHandlerMixin):
 
     def _blit(self, screen):
         screen.blit(self.world.bg, self.world.pos)
+        for s in self.world.sprites:
+            screen.blit(s.current_frame, s.pos)
