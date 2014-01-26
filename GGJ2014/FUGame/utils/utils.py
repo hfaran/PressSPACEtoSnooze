2bfa4ae070676f89c23e71c93ba1e79760047ac8
Dylan Dumesnil <dadumesnil@gmail.com>
2014-01-25 22:42:23 -0800

    Cell phone shows msgs and wraps msgs

--- GGJ2014/FUGame/utils/utils.py
+++ GGJ2014/FUGame/utils/utils.py
@@ -1,8 +1,7 @@
-import pygame
+from pygame import *
 
 
 class NextLevelException(Exception):
-
     def __init__(self, next_level, state):
         self.next_level = next_level
         self.state = state
@@ -16,3 +15,44 @@ def convert_white_to_transparent(image):
             if image.get_at((x, y)) == (255, 255, 255, 255):
                 image.set_at((x, y), (255, 255, 255, 0))
     return image
+
+# draw some text into an area of a surface
+# automatically wraps words
+# returns any text that didn't get blitted
+def drawText(surface, text, color, rect, font, aa=False, bkg=None):
+    rect = Rect(rect)
+    y = rect.top
+    lineSpacing = -2
+
+    # get the height of the font
+    fontHeight = font.size("Tg")[1]
+
+    while text:
+        i = 1
+
+        # determine if the row of text will be outside our area
+        if y + fontHeight > rect.bottom:
+            break
+
+        # determine maximum width of line
+        while font.size(text[:i])[0] < rect.width and i < len(text):
+            i += 1
+
+        # if we've wrapped the text, then adjust the wrap to the last word
+        if i < len(text):
+            i = text.rfind(" ", 0, i) + 1
+
+        # render the line and blit it to the surface
+        if bkg:
+            image = font.render(text[:i], 1, color, bkg)
+            image.set_colorkey(bkg)
+        else:
+            image = font.render(text[:i], aa, color)
+
+        surface.blit(image, (rect.left, y))
+        y += fontHeight + lineSpacing
+
+        # remove the text we just blitted
+        text = text[i:]
+
+    return text
