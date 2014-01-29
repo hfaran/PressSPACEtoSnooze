629c8feb054552056ad0b3b2bd0ae585ca4170aa
hfaran <hamzafaran@hotmail.com>
2014-01-28 22:00:38 -0800

    Add comments for World and SwagWord

--- PressSPACEtoSnooze/FUGame/swagwords.py
+++ PressSPACEtoSnooze/FUGame/swagwords.py
@@ -19,28 +19,26 @@ class SwagWord(object):
         self.gen_new_color()
 
     def gen_new_color(self):
+        """Set `self.color` to a new randomly generated color"""
         self.color = tuple([randint(0, 255) for i in xrange(3)])
 
     def gen_new_rect(self):
+        """Set `self.rect` to a new random position with bounds"""
         self.rect = pygame.Rect(
             randint(*self._X_BOUNDS), randint(*self._Y_BOUNDS),
-            400, 50)
+            400, 50
+        )
 
     def draw(self, screen):
+        """Blits SwagWord to `screen`
+
+        - Also calls `gen_new_color` at frequency `self.freq` to set new color
+        """
         self.fc = (self.fc + 1) % self.freq
         if self.fc < self.freq / 2:
             self.gen_new_color()
 
-        # self.fc = (self.fc + 1) % self.freq
-        # if self.fc < self.freq / 2:
         screen.blit(
-                self.font.render(self.word, True, self.color), (self.rect.x, self.rect.y)
-                )
-        # utils.drawText(
-        #     screen,
-        #     self.word,
-        #     self.color,
-        #     self.rect,
-        #     self.font,
-        #     aa=True
-        # )
+            self.font.render(self.word, True,
+                             self.color), (self.rect.x, self.rect.y)
+        )
